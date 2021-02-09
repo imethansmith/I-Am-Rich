e9100ea276fcfd7e708f590195e8eb135fc3441a
Ethan Smith <edhsmith@icloud.com>
2021-02-09 16:33:01 +1300

    Initial Commit

---
+++ I-Am-Rich-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-Am-Rich-2
+//
+//  Created by Ethan Smith on 9/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
